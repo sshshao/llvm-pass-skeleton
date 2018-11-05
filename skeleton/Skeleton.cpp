8fb9315858526816103d53e69577b1150946df93
S Shao <sshao14@gmail.com>
2018-11-05 15:53:40 -0500

    bug fixes

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -44,10 +44,10 @@ namespace {
             }
 
             for (std::pair<BasicBlock*, int> block : map) {
-                for (BasicBlock *Pred : predecessors(block.first)) {
-                    if (dTree->dominates(Pred, block.first)) {
+                for (BasicBlock *Succ : successors(block.first)) {
+                    if (dTree->properlyDominates(Succ, block.first)) {
                         errs() << "Loop " << loopCnt << "\t";
-                        errs() << "Loop detected: BasicBlock " << block.second << " goes back to Block " << map.at(Pred) << "\n";
+                        errs() << "Loop detected: BasicBlock " << block.second << " goes back to Block " << map.at(Succ) << "\n";
 
                         loopCnt++;
                     }
