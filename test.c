d4abb32514d1087d7576c522d5a68e6363d079e4
S Shao <sshao14@gmail.com>
2018-12-10 22:38:20 -0500

    modified test

--- test.c
+++ test.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
     }
 
     for(int i = 0; i < 20; i++) {
-        for(int j = 0; j < 5; j++) {
+        for(int j = i; j < 5; j++) {
             a++;
         }
     }
