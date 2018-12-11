1d1412a66b9a1f37768ac2f67f1c6cb7b73c52a1
S Shao <sshao14@gmail.com>
2018-12-10 21:52:37 -0500

    modified test

--- test.c
+++ test.c
@@ -2,28 +2,26 @@ int timesTwo(int num, int iteration);
 
 int main(int argc, char **argv) {
     int a = 0;
+
     for (int i = 0 ; i < 20 ; i *= 2) {
         for (int j = 0; j < 15; j++) {
             a++;
         }
     }
 
-    a = 0;
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < i; j++) {
             a--;
         }
     }
 
-    a = 0;
     for(int i = 0; i < 20; i++) {
         timesTwo(a, 50);
-        for(int j = 0; j < 5; j++) {
+        for(int j = i; j < 5; j++) {
             a++;
         }
     }
 
-    a = 0;
     for(int i = 0; i < 20; i++) {
         for(int j = 0; j < 5; j++) {
             a++;
