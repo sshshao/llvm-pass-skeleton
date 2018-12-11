ccf98135650b58f62eabd202734ae63b6ea49d7a
S Shao <sshao14@gmail.com>
2018-12-10 23:22:24 -0500

    modified test

--- test.c
+++ test.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
     }
 
     for(int i = 0; i < 20; i++) {
-        for(int j = 0; j < 5; j++) {
+        for(int j = 0; j < 5-i; j++) {
             a++;
         }
         timesTwo(a, 100);
